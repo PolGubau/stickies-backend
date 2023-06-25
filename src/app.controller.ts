93a9a2cee59da3a178ef665cf57768e4bffc5d73
Pol Gubau Amores <gubaupol@gmail.com>
2023-06-25 22:40:12 +0200

    Pol Gubau; changing swagger route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Hello :)') // Add the HTTP tag here
 // This is the app controller, the function of the controller is to handle requests and return responses, it is the middleman between the client and the server.
 @Controller()
 export class AppController {

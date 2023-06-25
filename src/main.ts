225f87648ad4e116bb20be0ca062e64ea3ac468b
Pol Gubau Amores <gubaupol@gmail.com>
2023-06-25 23:57:52 +0200

    Pol Gubau; MW-

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
+bootstrap();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import * as swaggerUi from 'swagger-ui-dist';
 
 async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
   const config = new DocumentBuilder()
     .setLicense('MIT', 'https://opensource.org/licenses/MIT')
     .setTermsOfService('https://www.polgubau.com/terms')
@@ -16,19 +19,12 @@ async function bootstrap() {
     .setContact('Pol', 'https://www.polgubau.com', 'gubaupol@gmail.com')
     .setBasePath('swagger')
     .build();
-
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, document, {
+  SwaggerModule.setup('api', app, document, {
     swaggerOptions: {
-      url: '/api-docs-json', // Endpoint to serve the Swagger JSON file
+      url: '/swagger/json', // Endpoint to serve the Swagger JSON file
     },
-    customCss: swaggerUi.getAbsoluteFSPath(),
-    customJs: swaggerUi.getAbsoluteFSPath(),
   });
-
-  // Serve static files from the "public" directory
-  app.useStaticAssets(join(__dirname, '..', 'public'));
 
   await app.listen(3000);
 }

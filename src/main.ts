b09e7c54e8bb56d590e97fe3ca0912fe2fe46a25
Pol Gubau Amores <gubaupol@gmail.com>
2023-06-26 00:03:35 +0200

    Pol Gubau; MW-

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,29 @@
-bootstrap();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import * as swaggerUi from 'swagger-ui-dist';
 
 async function bootstrap() {
+  const config = new DocumentBuilder()
+    // ... your existing code ...
+    .build();
+
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'public'));
 
-  const config = new DocumentBuilder()
-    .setLicense('MIT', 'https://opensource.org/licenses/MIT')
-    .setTermsOfService('https://www.polgubau.com/terms')
-    .addBearerAuth()
-    .setTitle('Stickies API')
-    .setDescription('The Stickies API description')
-    .setVersion('0.1')
-    .setContact('Pol', 'https://www.polgubau.com', 'gubaupol@gmail.com')
-    .setBasePath('swagger')
-    .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document, {
+  SwaggerModule.setup('swagger', app, document, {
     swaggerOptions: {
-      url: '/swagger/json', // Endpoint to serve the Swagger JSON file
+      // Specify the URL to the Swagger UI distribution file
+      url: '/api/swagger.json',
     },
+    customCss: swaggerUi.getAbsoluteFSPath(),
+    customJs: swaggerUi.getAbsoluteFSPath(),
   });
 
   await app.listen(3000);
 }
+
 bootstrap();

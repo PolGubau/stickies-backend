4e588d146658bd440f76ba45da5abc7e323f2078
Pol Gubau Amores <gubaupol@gmail.com>
2023-06-25 17:00:57 +0200

    Pol Gubau; Configuring app

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 // This is the app controller, the function of the controller is to handle requests and return responses, it is the middleman between the client and the server.
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
 }
